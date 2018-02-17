5fe49fe49f7547dcd5840472e26cb80c99832499
djallel bachkhaznadji <joedjallel@hotmail.com>
2018-02-17 21:33:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(bord)
+  puts" #{bord[0]} | #{bord[1]} | #{bord[2]}"
+  puts"-----------"
+  puts" #{bord[3]} | #{bord[4]} | #{bord[5]}"
+  puts"-----------"
+  puts" #{bord[6]} | #{bord[7]} | #{bord[8]}"
+end
